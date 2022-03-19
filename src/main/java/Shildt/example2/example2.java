3dc14305d8f3a27454a1b84f29633c3d6d18eab8
d1mon98 <ieshkin@yahoo.com>
2022-03-19 19:59:02 +0300

    add FirstProgam3

--- src/main/java/Shildt/example2/example2.java
+++ src/main/java/Shildt/example2/example2.java
@@ -1,15 +1,15 @@
-package Shildt.exapmple2;
+package Shildt.example2;
 
 
 public class example2 {
     public static void main(String[] args) {
         int num;
-        num = 100;
+        num = 99;
         System.out.println("Это переменная num: " + num);
-
+        if (num < 100) System.out.println("num меньше 100");
         num = num * 2;
 
-        System.out.println("Значение переменной num * 2 равно ");
+        System.out.print("Значение переменной num * 2 равно ");
         System.out.println(num);
     }
 }
