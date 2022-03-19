5018768f5b315c2a1a02486d9976c2726487a0f9
d1mon98 <ieshkin@yahoo.com>
2022-03-19 19:18:25 +0300

    add FirstProgam3

---
+++ src/main/java/Shildt/exapmple2/example2.java
@@ -0,0 +1,15 @@
+package Shildt.exapmple2;
+
+
+public class example2 {
+    public static void main(String[] args) {
+        int num;
+        num = 100;
+        System.out.println("Это переменная num: " + num);
+
+        num = num * 2;
+
+        System.out.println("Значение переменной num * 2 равно ");
+        System.out.println(num);
+    }
+}
