b3318f92c656cf5e6b83a54bf8bd55cac837f4c1
d1mon98 <ieshkin@yahoo.com>
2022-03-21 21:30:37 +0300

    add FirstProgam3

---
+++ src/main/java/itpark/lesson2/SecondProgram.java
@@ -0,0 +1,49 @@
+package itpark.lesson2;
+
+public class SecondProgram {
+    public static void main(String[] args) {
+
+        long summa = summa(3, 9);
+        System.out.println(summa);
+        long subtaction = minus(3,4);
+        System.out.println(subtaction);
+        long result = multyplyAndsumma(55);
+        System.out.println(result);
+        long result2 = formula(5,299792458);
+        System.out.println(result2);
+
+
+    }
+    /**
+     * Метод будет складывать два аргумента и возвращать результат этой операции
+     * @param arg1 первый аргумент
+     * @param arg2 второй аргумент
+     * @return результат сложения двух чисел
+     */
+    public static long summa(int arg1, int arg2) {
+    /*В этой переменной будет хранится результат сложения двух чисел.
+    Результат вычисления должен быть совместим по типу с аргументами,
+    над которыми осуществляются операции сложения */
+        return arg1 + arg2;
+    }
+
+    public static long minus(int arg1, int arg2){
+        return arg1 - arg2;
+    }
+
+    public static long multyplyAndsumma(int arg1) {
+        return 3L *arg1 +15;
+    }
+
+    /**
+     * Метод будет складывать два аргумента и возвращать результат этой операции
+     * @param m масса объекта
+     * @param c скорость света в вакууме = 299792458 м/с2
+     * @return энергия объекта
+     */
+    public static long formula (int m, int c){
+        return (long) m *c*c;
+    }
+
+}
+
