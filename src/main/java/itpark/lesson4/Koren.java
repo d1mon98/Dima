376b584e13dc0b2a006cc09bff8eaaf2c0eaf8f0
d1mon98 <ieshkin@yahoo.com>
2022-03-28 22:18:51 +0300

    square root resolve

--- src/main/java/itpark/lesson4/Koren.java
+++ src/main/java/itpark/lesson4/Koren.java
@@ -6,7 +6,7 @@ public class Koren {
 
     public static void main(String[] args) {
         int i;
-        System.out.println("Введите целое число, из которого вы хотите вычислить квадратный корень: ");
+        System.out.println("Введите целое число, из которого Вы хотите вычислить квадратный корень: ");
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         for (i = 1; i * i <= n; i++) {
