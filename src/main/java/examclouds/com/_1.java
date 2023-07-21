2d240a6c49198a1f015f932c208a2fa5b04ecccb
d1mon98 <93488624+d1mon98@users.noreply.github.com>
2023-07-21 20:01:13 +0300

    lesson 5 homework

---
+++ src/main/java/examclouds/com/_1.java
@@ -0,0 +1,35 @@
+/*
+1. Четное-нечетное.
+
+Передать на вход программы число в качестве аргумента.
+Если оно нечетное, распечатать его.
+Используем оператор if.
+Используйте метод Integer.parseInt() для преобразования из String в int.
+ */
+
+package examclouds.com;
+import java.util.Scanner;
+public class _1 {
+
+    //проверка на четность
+
+    static void ostatok(int a) {
+
+        int ostatok = a % 2;
+        if (ostatok == 0) {
+            System.out.println("Number is even");
+        } else {
+            System.out.println("Number is not even");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input digit: ");
+        String digit = input.next();
+        ostatok(Integer.parseInt(digit));
+
+    }
+
+}
---
+++ src/main/java/examclouds/com/_2.java
@@ -0,0 +1,32 @@
+/*
+С помощью класса Scanner ввести целое число.
+Если это число от 0 до 10, вывести на консоль сообщение “Положительное число меньше 10 или ноль”,
+иначе вывести сообщение “Положительное число больше 10 или отрицательное”.
+Используем оператор if-else.
+ */
+
+package examclouds.com;
+
+import java.util.*;
+
+public class _2 {
+    static void checkNumber(int a) {
+
+        if (a >= 0 && a <= 10) {
+            System.out.println("Если это число от 0 до 10");
+        } else {
+            System.out.println("Положительное число больше 10 или отрицательное");
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        String digit = input.next();
+
+        checkNumber(Integer.parseInt(digit));
+    }
+
+}
