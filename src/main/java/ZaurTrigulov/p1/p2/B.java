25c2a1d34c23d1774b25ac48608c8351c48cca96
d1mon98 <ysudhxesQop-vusgix-1zykvuHyr%#>
2023-07-21 15:23:14 +0300

    21.07.2023

---
+++ src/main/java/ZaurTrigulov/p1/p2/B.java
@@ -0,0 +1,12 @@
+package ZaurTrigulov.p1.p2;
+
+public class B {
+    static int a;
+    static boolean b;
+    public static String ss = "111!!";
+
+    public static void main(String[] args) {
+        System.out.println(ZaurTrigulov.p1.p2.B.ss);
+    }
+}
+
